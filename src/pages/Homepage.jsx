b6c29fdb82e672c75ec14025913fe567746ee609
darrenmfdes <darrenmfdes@gmail.com>
2022-01-03 01:34:21 +0530

    temp fix

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,12 +2,6 @@ import React from "react";
 import { useGetCryptosListQuery } from "../services/cryptoApi";
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosListQuery();
-
-  if (!isFetching) {
-    console.log(data);
-  }
-
   return <div></div>;
 };
 
