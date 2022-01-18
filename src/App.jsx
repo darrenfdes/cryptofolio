649757039e374807f99f9acb98b6cf7ecbae1238
darrenmfdes <darrenmfdes@gmail.com>
2022-01-18 12:21:55 +0530

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Homepage from "./pages/Homepage";
 import Cryptocurrency from "./pages/Cryptocurrency";
 import CryptoDetails from "./pages/CryptoDetails";
 import News from "./pages/News";
-import Footer from "./layout/Footer";
+// import Footer from "./layout/Footer";
 
 const useStyles = makeStyles((theme) => ({
   app: {
